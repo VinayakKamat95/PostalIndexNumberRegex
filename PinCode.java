25f6392a26b1c093dd41585c3648cec569fbdb19
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-12 22:04:29 +0530

    Added: Regex Pattern To Validate PINCode 400088

--- PinCode.java
+++ PinCode.java
@@ -1,11 +1,33 @@
-//Welcome to The Regex pattern to validate PIN code
+//!/bin/bash -x
 
+import java.util.regex.*;
 
 public class PinCode {
 
-	public static void main(String args[])
+   public static boolean isValidPinCode(String pinCode)
     {
-        System.out.println("Welcome to The Regex pattern to validate PIN code");
+
+        String regex
+            = "[1-9]{1}[0-9]{2}[0-9]{3}";
+
+        Pattern p = Pattern.compile(regex);
+
+        if (pinCode == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(pinCode);
+        return m.matches();
+    }
+
+   public static void main(String args[])
+    {
+
+        String num1 = "132103";
+        System.out.println(
+            num1 + ": "
+            + isValidPinCode(num1));
+
     }
 
 }

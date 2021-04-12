e06f81e11ff92dc9182e3f560ff80804b25b2bfb
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-12 21:53:27 +0530

    Added: Welcome To Regex pattern to validate PIN code

---
+++ PinCode.java
@@ -0,0 +1,11 @@
+//Welcome to The Regex pattern to validate PIN code
+
+
+public class PinCode {
+
+	public static void main(String args[])
+    {
+        System.out.println("Welcome to The Regex pattern to validate PIN code");
+    }
+
+}
